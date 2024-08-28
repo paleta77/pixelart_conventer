48501db197fc015ffd3a66cad61fb9b93cdcef12
Paweł Szrubas <pawel050673@gmail.com>
2024-08-28 18:59:45 +0200

    moved block_size to convert method

--- pixelart_conventer/pixelart_conventer.py
+++ pixelart_conventer/pixelart_conventer.py
@@ -5,9 +5,9 @@ def main():
     image_path = "image.jpg"
     block_size = 16
 
-    pixel_converter = converter.Converter(image_path, block_size)
+    pixel_converter = converter.Converter(image_path)
 
-    converted_image = pixel_converter.convert()
+    converted_image = pixel_converter.convert(block_size)
     converted_image.save("converted.png")
 
 
