b21ceba724523da80e95177e73ee955cc8369fd8
Paweł Szrubas <pawel050673@gmail.com>
2024-08-29 21:33:57 +0200

    added database
    fixed typo

--- pixelart_converter/asgi.py
+++ pixelart_converter/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pixelart_conventer project.
+ASGI config for pixelart_converter project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pixelart_conventer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pixelart_converter.settings')
 
 application = get_asgi_application()
