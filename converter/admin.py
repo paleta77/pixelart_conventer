951d0d5189d007aae2d71e5d23db9f7c8db629db
Paweł Szrubas <pawel050673@gmail.com>
2024-08-29 22:39:50 +0200

    added models

--- converter/admin.py
+++ converter/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from converter.models import Image, User
+
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(User)
--- converter/models.py
+++ converter/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    password_hash = models.CharField(max_length=256)
+
+    def __str__(self):
+        return f"{self.name} ({self.email})"
+
+
+class Image(models.Model):
+    block_size = models.IntegerField()
+    colors_number = models.IntegerField()
+    path = models.CharField(max_length=256)
+    created = models.DateTimeField(auto_now_add=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.path} ({self.creator})"
+
+    def is_recent(self):
+        return self.created > timezone.now() - datetime.timedelta(days=1)
